3baceb0f346b39d62511be67579209984bff20cd
Nobuhle01 <nobuhlentshangase21@gmail.com>
2025-04-29 15:16:58 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PlatformCredentialsComponent } from './platform-credentials/platform-credentials.component';
+import { ScrapingComponent } from './scraping/scraping.component';
+import { StartScrapingComponent } from './start-scraping/start-scraping.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {path: 'forgot-password', component: ForgotPasswordComponent},
+  {path : 'dashboard', component: DashboardComponent},
+  {path : 'platform-credentials', component: PlatformCredentialsComponent},
+  {path : 'scraping', component: ScrapingComponent},
+  {path : 'start-scraping', component: StartScrapingComponent}, 
+
+];
